640df860cb37707c69d3482476759d0e3812e7a6
José Luis Gómez <joseluisgomezpascual@gmail.com>
2013-09-07 23:23:16 +0200

    Sexto commit

--- main.c
+++ main.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
 #include<mysql/mysql.h>
 
+struct variables
+{
+	char *server;
+	char *user;
+	char *password;
+	char *bd;
+};
+	
 int main()
 {
 	MYSQL *con=mysql_init(NULL);
 	MYSQL_RES *res;
 	MYSQL_ROW row;
-	char *server="localhost";
-	char *user="root";
-	char *password="root";
-	char *bd="prueba";
+	struct variables var;
+	var.server="localhost";
+	var.user="root";
+	var.password="root";
+	var.bd="sakila";
 	
-	if(!mysql_real_connect(con,SERVER,USER,PASSWORD,BD,0,NULL,0))
+	if(!mysql_real_connect(con,var.server,var.user,var.password,var.bd,0,NULL,0))
 	{
 		printf("Error %s\n",mysql_errno(con));
 	}
-	mysql_query(con,"show processlist");
+	mysql_query(con,"select * from actor limit 10");
 	res=mysql_use_result(con);
 	while((row=mysql_fetch_row(res))!=NULL)
 	{
-		printf("%s \n",row[0]);
+		printf("%s %s %s\n",row[0],row[1],row[2]);
 	}
 	mysql_close(con);
 	return 0;
